d11c9754bfb76804f849d85344e5d8af46262673
2202anonymoususer <2202anonymoususer@gmail.com>
2020-01-23 11:55:58 +0100

    Refactored Game object creation with before annotation

--- src/test/java/com/kata/tictactoe/GameTest.java
+++ src/test/java/com/kata/tictactoe/GameTest.java
@@ -1,23 +1,31 @@
 package com.kata.tictactoe;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class GameTest {
-    @Test
-    public void givenAPositionWhenFirstTurnThenGameShouldMarkX(){
-        Game game = new Game();
-        Position firstPosition = new Position(0,0);
-        game.playAt(firstPosition);
-        assertEquals('X',game.getTokenAt(firstPosition));
+    private Game game;
+
+    @Before
+    public final void before() {
+        game = new Game();
     }
+
     @Test
-    public void givenAPositionValueWhenSecondTurnThenGameShouldMarkO(){
-        Game game = new Game();
-        Position firstPosition = new Position(0,0);
-        Position secondPosition = new Position(0,1);
+    public void givenAPositionWhenFirstTurnThenGameShouldMarkX() {
+        Position firstPosition = new Position(0, 0);
+        game.playAt(firstPosition);
+        assertEquals('X', game.getTokenAt(firstPosition));
+    }
+
+    @Test
+    public void givenAPositionValueWhenSecondTurnThenGameShouldMarkO() {
+        Position firstPosition = new Position(0, 0);
+        Position secondPosition = new Position(0, 1);
         game.playAt(firstPosition);
         game.playAt(secondPosition);
-        assertEquals('O',game.getTokenAt(secondPosition));
+        assertEquals('O', game.getTokenAt(secondPosition));
     }
 }
