304956848880edb9b3dc4700ff03bef0be90c974
2202anonymoususer <2202anonymoususer@gmail.com>
2020-01-23 12:00:43 +0100

    Refactored to use row and column for position creation

--- src/main/java/com/kata/tictactoe/Position.java
+++ src/main/java/com/kata/tictactoe/Position.java
@@ -3,14 +3,16 @@ package com.kata.tictactoe;
 public class Position {
     private int row;
     private int column;
+
     public Position(int row, int column) {
         this.row = row;
         this.column = column;
     }
 
-    public Position() {
+    private Position() {
 
     }
+
     public int getRow() {
         return row;
     }
