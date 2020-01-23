4ae2df8b3ad800940072a0b788aef6b8fcd34ecb
2202anonymoususer <2202anonymoususer@gmail.com>
2020-01-23 11:37:20 +0100

    Refactored the test case for player1 to choose a position on gird

--- src/test/java/com/kata/tictactoe/GameTest.java
+++ src/test/java/com/kata/tictactoe/GameTest.java
@@ -6,20 +6,18 @@ import static org.junit.Assert.assertEquals;
 public class GameTest {
     @Test
     public void givenAPositionWhenFirstTurnThenGameShouldMarkX(){
-        GameBoard gameboard = new GameBoard();
-        Position position = new Position();
         Game game = new Game();
-        game.playAt(position);
-        assertEquals("X",gameboard.getTokenAt(position));
+        Position firstPosition = new Position(0,0);
+        game.playAt(firstPosition);
+        assertEquals('X',game.getTokenAt(firstPosition));
     }
     @Test
     public void givenAPositionValueWhenSecondTurnThenGameShouldMarkO(){
+        Game game = new Game();
         Position firstPosition = new Position(0,0);
         Position secondPosition = new Position(0,1);
-        Game game = new Game();
         game.playAt(firstPosition);
         game.playAt(secondPosition);
         assertEquals('O',game.getTokenAt(secondPosition));
     }
-
 }
