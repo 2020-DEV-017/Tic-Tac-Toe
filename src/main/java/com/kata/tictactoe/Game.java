7c8645ddbb42082ed836e985a85d431d0e149e85
2202anonymoususer <2202anonymoususer@gmail.com>
2020-01-23 17:32:09 +0100

    Games results are displayed to the players by print the game board in console.

--- src/main/java/com/kata/tictactoe/Game.java
+++ src/main/java/com/kata/tictactoe/Game.java
@@ -14,6 +14,8 @@ public class Game {
 
     public String playAt(Position position) {
         if (isPositionNotAvailable(position)) {
+            displayBoard();
+            System.out.println("Position is Occupied");
             throw new RuntimeException("Position is Occupied");
         }
         char token = getNextToken();
@@ -24,6 +26,8 @@ public class Game {
             return lastPlayerToken + " is the Winner";
         }
         if (isDraw()) {
+            displayBoard();
+            System.out.println("Game is draw");
             return "Game is draw";
         }
         return null;
@@ -49,6 +53,10 @@ public class Game {
         for (int rows = 0; rows < gridlayout[0].length; rows++) {
             if (gridlayout[rows][0] == gridlayout[rows][1] && gridlayout[rows][1] == gridlayout[rows][2]
                     && gridlayout[rows][0] != EMPTY_SPACE) {
+                if (gridlayout[rows][0] == 'X' || gridlayout[rows][0] == 'O') {
+                    displayBoard();
+                    System.out.println(gridlayout[rows][0] + " is the Winner");
+                }
                 return true;
             }
         }
@@ -60,6 +68,10 @@ public class Game {
         for (int columns = 0; columns < gridlayout[0].length; columns++) {
             if (gridlayout[0][columns] == gridlayout[1][columns] && gridlayout[1][columns] == gridlayout[2][columns]
                     && gridlayout[0][columns] != EMPTY_SPACE) {
+                if (gridlayout[0][columns] == 'X' || gridlayout[0][columns] == 'O') {
+                    displayBoard();
+                    System.out.println(gridlayout[0][columns] + " is the Winner");
+                }
                 return true;
             }
         }
@@ -70,6 +82,10 @@ public class Game {
         char[][] gridlayout = gameBoard.getLayout();
         if (gridlayout[0][0] == gridlayout[1][1] && gridlayout[1][1] == gridlayout[2][2]
                 && gridlayout[0][0] != EMPTY_SPACE) {
+            if (gridlayout[0][0] == 'X' || gridlayout[0][0] == 'O') {
+                displayBoard();
+                System.out.println(gridlayout[0][0] + " is the Winner");
+            }
             return true;
         }
         return false;
@@ -79,6 +95,10 @@ public class Game {
         char[][] gridlayout = gameBoard.getLayout();
         if (gridlayout[0][2] == gridlayout[1][1] && gridlayout[1][1] == gridlayout[2][0]
                 && gridlayout[0][2] != EMPTY_SPACE) {
+            if (gridlayout[0][2] == 'X' || gridlayout[0][2] == 'O') {
+                displayBoard();
+                System.out.println(gridlayout[0][2] + " is the Winner");
+            }
             return true;
         }
         return false;
@@ -94,5 +114,23 @@ public class Game {
             }
         }
         return true;
+    }
+
+    public void displayBoard() {
+        char[][] gridlayout = gameBoard.getLayout();
+        System.out.println();
+        for (int row = 0; row < SIZE; ++row) {
+            for (int column = 0; column < SIZE; ++column) {
+                System.out.print("\t" + gridlayout[row][column]);
+                if (column != SIZE - 1) {
+                    System.out.print(" |");
+                }
+            }
+            System.out.println();
+            if (row != SIZE - 1) {
+                System.out.println("------------------");
+            }
+        }
+        System.out.println();
     }
 }
